397e868f14f2a3c0343f9fda1e83a110ad289d52
Daniel Nilsson-Cole <dnilssoncole@gmail.com>
2017-07-26 09:06:22 -0400

    form validation

--- app/survey/survey.ctrl.js
+++ app/survey/survey.ctrl.js
@@ -6,17 +6,35 @@ export default function SurveyController ($scope, $location) {
     form.$setPristine();
   }
 
-  $scope.testFunc = (form) => {
+  $scope.isTempValid = (form) => {
     window.console.log(form);
     window.console.log(form.CurrentTemp.$modelValue);
-  }
-
-  $scope.isTempValid = (field, value) => {
-    if (value > 200) {
-      field.$setValidity("aboveMax", false);
-    } else if (value < -100) {
-      field.$setValidity("belowMin", false);
+    let value = form.CurrentTemp.$modelValue;
+    let maxTemp = 200;
+    let minTemp = -100;
+    if (value > maxTemp) {
+      form.CurrentTemp.$setValidity("aboveMax", false);
+    } else if (value < minTemp) {
+      form.CurrentTemp.$setValidity("belowMin", false);
+    } else if (value > minTemp && value < maxTemp) {
+      form.CurrentTemp.$setValidity("belowMin", true);
+      form.CurrentTemp.$setValidity("aboveMax", true);
     }
   }
 
+  $scope.isPercentValid = (form, field) => {
+    window.console.log(form);
+    window.console.log(form.field.$modelValue);
+    let value = form.CurrentTemp.$modelValue;
+    let max = 100;
+    let min = 0;
+    if (value > max) {
+      form.field.$setValidity("aboveMax", false);
+    } else if (value < min) {
+      form.field.$setValidity("belowMin", false);
+    } else if (value > min && value < max) {
+      form.field.$setValidity("belowMin", true);
+      form.field.$setValidity("aboveMax", true);
+    }
+  }
 }
